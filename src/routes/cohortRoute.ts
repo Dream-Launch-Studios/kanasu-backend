69fa701262b1fda09536ab8394626549c6901c6b
gitcuber369 <arpitchaudhary2003@gmail.com>
2025-04-04 00:10:47 +0530

    [update]:anganwadi CRUD

--- src/routes/cohortRoute.ts
+++ src/routes/cohortRoute.ts
@@ -1,11 +1,10 @@
 import express from "express";
-import { createCohort, getCohorts, deleteCohort  } from "../controllers/cohortController";
+import { createCohort, deleteCohort, getCohorts,  } from "../controllers/cohortController";
 
 const router = express.Router();
 
 router.post("/", createCohort);
 router.get("/", getCohorts);
-
 router.delete("/:id", deleteCohort);
 
 export default router;
